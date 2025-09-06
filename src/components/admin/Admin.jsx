f84a3227da771e09e74d540ed5c1c9470267860f
Arun Das <arundas123@gmail.com>
2025-09-06 11:40:05 +0530

    Fix: corrected Body import path

--- src/components/admin/Admin.jsx
+++ src/components/admin/Admin.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Sidebar from "./Sidebar";
+import Sidebar from "./Sidebar.jsx";
 import Body from "./Body.jsx";
 import '../css/body.css';
 import { FaBell } from "react-icons/fa";
